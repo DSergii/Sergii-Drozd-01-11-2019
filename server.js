6659c6367bdb7d381bf0553597f64cc99216e410
DSergii <drozdsa@gmail.com>
2019-11-04 10:39:07 +0200

    change server js settings

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static('./dist'));
+app.use(express.static(__dirname + '/dist'));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
