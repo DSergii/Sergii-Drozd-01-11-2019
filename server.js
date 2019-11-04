8d17416c5bad5a0c672c11107eb2dad33035338f
DSergii <drozdsa@gmail.com>
2019-11-04 10:04:54 +0200

    change build folder

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/weather-app'));
-
+app.use(express.static(__dirname + '/dist'));
+console.log('server run');
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/weather-app/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // default Heroku PORT
